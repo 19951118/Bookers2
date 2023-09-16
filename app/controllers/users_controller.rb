a0426060d44d706e8123891ca5884bad033302d6
19951118 <reonn1118@yahoo.co.jp>
2023-09-16 16:41:49 +0000

    indexページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
   def edit
+    @user = User.find(params[:id])
   end
 
   def index
   end
   
   def show
+    @user = User.find(params[:id])
   end
   
   def update
+    @user = User.find(params[:id])
   end
 end
