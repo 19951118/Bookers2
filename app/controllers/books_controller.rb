2f876cd2bfaa933d83a9ef8894cd836ac801459c
19951118 <reonn1118@yahoo.co.jp>
2023-09-17 09:08:32 +0000

    bookのdestroy機能実装まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,4 @@
 class BooksController < ApplicationController
-  def new
-    
-  end
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -10,6 +7,17 @@ class BooksController < ApplicationController
     else
       render :index
     end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -22,13 +30,13 @@ class BooksController < ApplicationController
     @book_id = Book.find(params[:id])
     @book = Book.new
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to books_path
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
