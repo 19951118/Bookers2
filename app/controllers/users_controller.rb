d6505c9097fb669fafe70a7854608d5e131cc2b6
19951118 <reonn1118@yahoo.co.jp>
2023-09-17 14:21:06 +0000

    user/show作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,15 @@ class UsersController < ApplicationController
   end
 
   def index
+    @user = current_user
+    @users = User.all
+    @book = Book.new
   end
   
   def show
+    @book = Book.new
     @user = User.find(params[:id])
+    @books = @user.books
   end
   
   def update
