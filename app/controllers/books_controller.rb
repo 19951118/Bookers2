c1f371e603bf93f376445782d45135502cfc42cc
19951118 <reonn1118@yahoo.co.jp>
2023-09-18 10:31:58 +0000

    バリデーションの実装終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,11 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
+      @user = current_user
+      @books = Book.all
       render :index
     end
   end
@@ -16,9 +19,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to
-    book_path(@book.id)
+    if @book.update(book_params)
+      flash[:notice] = "You have update book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -15,4 +15,5 @@ class User < ApplicationRecord
     end
     profile_image.variant(resize_to_limit: [width,height]).processed
   end
+  validates :name, length: { minimum: 2 }
 end
